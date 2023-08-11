7ee1dd997b125267844afdc222543916cb9077ad
Matthias Leuffen <matthes@leuffen.de>
2023-08-11 02:10:06 +0200

    update

--- src/mediastore/MicxCdnImageObserver.ts
+++ src/mediastore/MicxCdnImageObserver.ts
@@ -7,14 +7,14 @@ export class MicxCdnImageObserver {
 
 
     private applyToImg(image : HTMLImageElement) {
+        if ((image as any)["micx_cdn_observer"] === true)
+            return;
+        (image as any)["micx_cdn_observer"] = true;
+
         if (image.src.indexOf("/v2/") === -1)
             return; // Not a CDN image
         if ( ! image.hasAttribute("micx_cdn_idx"))
             image.setAttribute("micx_cdn_idx", "" + cdnIdx++);
-
-        if ((image as any)["micx_cdn_observer"] === true)
-            return;
-        (image as any)["micx_cdn_observer"] = true;
 
         let e = new MicxCdnImgElement(image, parseInt(image.getAttribute("micx_cdn_idx")));
     }
@@ -23,11 +23,10 @@ export class MicxCdnImageObserver {
     public async observe() {
         let round = 1;
         while(true) {
-            await sleep(10 * round++);
-            await document.querySelectorAll("img").forEach(img => {
+            document.querySelectorAll("img").forEach(img => {
                 this.applyToImg(img);
             });
-
+            await sleep(10 * round++);
             if (round > 50)
                 round = 50;
         }
